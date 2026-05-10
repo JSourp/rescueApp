78f3389fb2a2a0c3a295ee1e7158d1c34b70c07f
Jon Sourp <jsourp@gmail.com>
2026-05-10 14:42:44 -0700

    removed donorbox default interval

--- rescue-app-frontend/src/app/layout.tsx
+++ rescue-app-frontend/src/app/layout.tsx
@@ -39,7 +39,7 @@ export default function RootLayout({
               id="donorbox-popup-button-installer"
               src="https://donorbox.org/install-popup-button.js"
               strategy="afterInteractive"
-              data-href="https://donorbox.org/second-chance-794835?default_interval=m"
+              data-href="https://donorbox.org/second-chance-794835"
               data-style="background: #fdac74; color: #fff; text-decoration: none; font-family: Verdana, sans-serif; display: flex; gap: 8px; width: fit-content; font-size: 16px; border-radius: 5px 5px 0 0; line-height: 24px; position: fixed; top: 50%; transform-origin: center; z-index: 9999; overflow: hidden; padding: 8px 22px 8px 18px; right: 20px; transform: translate(+50%, -50%) rotate(-90deg)"
               data-button-cta="Donate"
               data-img-src="https://donorbox.org/images/white_logo.svg"
