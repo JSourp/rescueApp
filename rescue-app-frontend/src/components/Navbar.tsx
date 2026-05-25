3d684945493fbe6021540bfcab1f38c66d9e2c56
Jon Sourp <jsourp@gmail.com>
2026-05-24 22:46:28 -0700

    replaced png logo with svg version

--- rescue-app-frontend/src/components/Navbar.tsx
+++ rescue-app-frontend/src/components/Navbar.tsx
@@ -91,7 +91,7 @@ export function Navbar() {
         {/* Logo */}
         <Link href="/">
           <Image
-            src="/img/placeholder-image.png"
+            src="/img/SCARS_Logo.svg"
             alt="Second Chance Animal Rescue & Sanctuary"
             width={250} // Keep intrinsic
             height={250}
